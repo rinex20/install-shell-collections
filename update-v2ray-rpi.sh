cdea7f014f0c666da5aab2aa26ffd03ab3a9135f
rinex20 <rinex20@me.com>
2020-04-20 11:13:25 +0800

    Update update-v2ray-rpi.sh

--- update-v2ray-rpi.sh
+++ update-v2ray-rpi.sh
@@ -2,7 +2,7 @@
 
 DOWNLOAD_URL="https://github.com/v2ray/v2ray-core/releases/download/"
 FILE_NAME="v2ray-linux-arm.zip"
-V2RAY_DIR="/usr/bin/v2ray/"
+V2RAY_DIR="/usr/bin/v2ray"
 
 mkdir v2ray -p
 wget -O ${FILE_NAME} ${DOWNLOAD_URL}$1/${FILE_NAME}
@@ -10,10 +10,10 @@ wget -O ${FILE_NAME} ${DOWNLOAD_URL}$1/${FILE_NAME}
 unzip -d v2ray ${FILE_NAME}
 
 systemctl stop v2ray
-cp v2ray/v2ray_armv7 V2RAY_DIR
-cp v2ray/v2ctl_armv7 V2RAY_DIR
-cp v2ray/geoip.dat V2RAY_DIR
-cp v2ray/geosite.dat V2RAY_DIR
+cp v2ray/v2ray_armv7 ${V2RAY_DIR}/v2ray
+cp v2ray/v2ctl_armv7 ${V2RAY_DIR}/v2ctl
+cp v2ray/geoip.dat ${V2RAY_DIR}/
+cp v2ray/geosite.dat ${V2RAY_DIR}/
 
 systemctl start v2ray
 systemctl status v2ray
